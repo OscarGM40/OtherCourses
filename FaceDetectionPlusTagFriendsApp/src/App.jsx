98192735ad71bed6b39f791a2afa42f179f45e53
Oscar <oscargm28@hotmail.com>
2021-12-03 14:24:26 +0100

    finished ai face detection

--- FaceDetectionPlusTagFriendsApp/src/App.jsx
+++ FaceDetectionPlusTagFriendsApp/src/App.jsx
@@ -1,40 +1,81 @@
-import { useEffect, useRef } from 'react';
-import * as faceapi from 'face-api.js';
+import { useEffect, useState } from 'react';
 import './app.css';
+import { Navbar } from './components/Navbar';
+import NewPost from './components/NewPost';
 
 function App() {
 
-  const imgRef = useRef();
-  const canvasRef = useRef();
+  const [ file,setFile ] = useState();
+  const [ image,setImage ] = useState();
   
+  // este efecto se dispara cada vez que haya un file.No tiene nada que ver con el submit.Fijate que necesito la altura y ancho de la imagen para el canvas
   useEffect(() => {
-    const loadModels = () => {
-      Promise.all([
-      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),  
-      faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-      faceapi.nets.faceExpressionNet.loadFromUri('/models')
-      ])
-        .then(console.log('finished loading models'))
-        .catch(err => console.log(err));
-    }
-    imgRef.current && loadModels();
-  },[])
+
+    const getImage = () => {
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      // en el onload porque hay que esperar a que la imagen este cargada(mejor lo hubieran llamado onFinishingLoad)
+      img.onload = () => {
+      setImage({
+        url: img.src,
+        width: img.width,
+        height: img.height
+      });
+    }}
+    file && getImage();    
+  },[file]);
+
+  console.log(image);
   
   return (
     <div className="app">
-      <img
-        ref={imgRef}
-        src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt=""
-        width="940"
-        height="650" />
+      <Navbar />
+      { image ? (
+        <NewPost image={image} />
+      ) : (
 
-      <canvas
-        ref={canvasRef}
-        width="940"
-        height="650" />
-
+        <div className="newPostCard">
+        <div className="addPost">
+          <img
+            src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="avatar"
+            />
+          <div className="postForm">
+            <input
+              type="text"
+              placeholder="What's on your mind?"
+              className="postInput"
+              />
+            <input
+              onChange={(e) => setFile(e.target.files[0])}
+              id="file"
+              style={{ display: "none" }}
+              type="file"
+              /> 
+            <label htmlFor="file">
+            
+              <img
+                className="addImg"
+                src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
+                alt=""
+                />
+              <img
+                className="addImg"
+                src="https://icon-library.com/images/maps-icon-png/maps-icon-png-5.jpg"
+                alt=""
+                />
+              <img
+                className="addImg"
+                src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
+                alt=""
+                /> 
+              <button>Send</button>
+            </label>
+          </div>
+        </div>
+      </div>
+    )}
     </div>
   );
 }
